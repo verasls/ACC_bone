fc1aa40973cdf6c3f1b295e8ef54a553fd46ce1c
Lucas Veras <lucasdsveras@gmail.com>
2019-10-28 10:09:15 +0000

    Change time data from datetime to interger and add axes labels

--- code/test.py
+++ code/test.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("../data/ID_119_hip_RAW.csv", delimiter=",", skiprows=10)
 
-print("Initiating conversion from string to datetime")
-data["Timestamp"] = pd.to_datetime(data["Timestamp"])
-print("Conversion done!")
+# print("Initiating conversion from string to datetime")
+# data["Timestamp"] = pd.to_datetime(data["Timestamp"])
+# print("Conversion done!")
 
-time = data.iloc[:, 0]
+time = range(0, len(data))
 resultant = np.sqrt((data.iloc[:, 1] ** 2) +
                     (data.iloc[:, 2] ** 2) +
                     (data.iloc[:, 3] ** 2))
 
 plt.plot(time, resultant)
+plt.xlabel("Time (cs)")
+plt.ylabel("Resultant acceleration (g)")
 plt.show()
