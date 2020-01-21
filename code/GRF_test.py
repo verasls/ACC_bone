6e5e421fe78543938d16dec8b55804f51be8e37a
Lucas Veras <lucasdsveras@gmail.com>
2020-01-21 17:10:48 +0000

    Mark first and last peaks of each bout in the plot

--- code/GRF_test.py
+++ code/GRF_test.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # Read data
 path = "../data/119/2017-12-09_Jumps_5cm_1_m1_119.txt"
 data = np.loadtxt(path, delimiter=",")
-samp_freq = 1000
+samp_freq = 1000  # sample frequency (Hz)
 
 # Get variables of interest
 body_mass = 74  # subject's mass (kg)
@@ -18,7 +18,7 @@ body_weight = body_mass * g  # body weight (BW; N)
 
 force = data[:, 2]  # ground reaction force (GRF; N)
 time = np.array(range(1, len(force) + 1))
-time = time / 1000
+time = time / samp_freq  # time in seconds
 
 # Filter GRF data
 # Create the lowpass filter
@@ -40,6 +40,7 @@ height = 3 * mean(force_BW)
 distance = 0.4 * samp_freq  # seconds * sampling frequency
 peaks, properties = signal.find_peaks(force_BW, height=height,
                                       distance=distance)
+time_of_peaks = peaks / samp_freq
 
 # Divide bouts
 print("Peaks found:", peaks)
@@ -52,11 +53,19 @@ print("Max width:", max(widths))
 max_width = widths.index(max(widths)) + 1
 print("Peak with max width:", peaks[max_width])
 
+# Mark first and last peaks of the bouts
+first_peaks = np.array([peaks[0], peaks[max_width]])
+last_peaks = np.array([peaks[max_width - 1], peaks[len(peaks) - 1]])
+time_first_peaks = first_peaks / samp_freq
+time_last_peaks = last_peaks / samp_freq
+
 # Plot
 fig = plt.figure(figsize=(15, 7))
 ax1 = fig.add_subplot(1, 1, 1)
 ax1.plot(time, force_BW)
-ax1.plot(peaks / 1000, force_BW[peaks], "x")
+ax1.plot(time_of_peaks, force_BW[peaks], "x")
+ax1.plot(time_first_peaks, force_BW[first_peaks], "x")
+ax1.plot(time_last_peaks, force_BW[last_peaks], "x")
 plt.xticks(np.arange(min(time), max(time) + 1, 5))
 plt.yticks(np.arange(0, 6, 0.5))
 plt.grid()
