5a341eeb7fb4773c222a814d484da1e398b294f1
Lucas <lucasdsveras@gmail.com>
2020-10-30 14:19:29 +0000

    Get and tidy running data

--- code/01_tidy_data.R
+++ code/01_tidy_data.R
@@ -1,6 +1,7 @@
 # Load packages -----------------------------------------------------------
 
 library(tidyverse)
+library(readxl)
 library(janitor)
 library(lvmisc)
 
@@ -11,6 +12,61 @@ anthropometric <- read_csv("data/anthropometric_data.csv") %>%
   mutate(
     BMI = bmi(body_mass, height / 100),
     BMI_cat = bmi_cat(BMI)
+  )
+
+# Obtain running data -----------------------------------------------------
+
+running_df_wide <- read_xlsx("data/GRF_ACC_data_all.xlsx", sheet = "all") %>% 
+  clean_names() %>% 
+  select(
+    id, trial = visita, filename = nome, acc_placement = local_acelerometro,
+    speed = velocidade_km_h, 
+    res_n_peaks = ftot_numero_picos_ftot,
+    ver_n_peaks = fvt_numero_picos_fvt,
+    pRACC_g = atot_med_picos_atot_g,
+    pRGRF_N = ftot_med_picos_ftot_n,
+    pRGRF_BW = ftot_med_picos_ftot_bw,
+    pVACC_g = avt_med_picos_avt_g,
+    pVGRF_N = fvt_med_picos_fvt_n,
+    pVGRF_BW = fvt_med_picos_fvt_bw
+  ) %>% 
+  filter(speed > 6 & acc_placement %in% c("ankle", "back", "waist"))
+
+running_df_res <- running_df_wide %>% 
+  select(
+    id, trial, filename, acc_placement, speed,
+    n_peaks = res_n_peaks, pACC_g = pRACC_g, 
+    pGRF_N = pRGRF_N, pGRF_BW = pRGRF_BW
+  ) %>% 
+  mutate(
+    vector = "resultant",
+    .after = acc_placement
+  )
+
+running_df_ver <- running_df_wide %>% 
+  select(
+    id, trial, filename, acc_placement, speed,
+    n_peaks = ver_n_peaks, pACC_g = pVACC_g, 
+    pGRF_N = pVGRF_N, pGRF_BW = pVGRF_BW
+  ) %>% 
+  mutate(
+    vector = "vertical",
+    .after = acc_placement
+  )
+
+running_df <- rbind(running_df_res, running_df_ver) %>% 
+  mutate(
+    # Create another subj identifier
+    subj = paste0(id, "m", trial),
+    subj = ifelse(str_length(subj) == 3, paste0("00", subj), subj),
+    subj = ifelse(str_length(subj) == 4, paste0("0", subj), subj)
+  ) %>% 
+  left_join(
+    select(anthropometric, id, trial, body_mass, BMI, BMI_cat),
+    by = c("id", "trial")
+  ) %>% 
+  select(
+    id, trial, filename, subj, body_mass, BMI, BMI_cat, everything()
   )
 
 # Obtain jumping data -----------------------------------------------------
