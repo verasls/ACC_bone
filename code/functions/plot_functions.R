d6d963283371482053b36ccb9295f29ba2fd45c7
Lucas <lucasdsveras@gmail.com>
2020-11-02 11:02:17 +0000

    Add box_plot() and is_outlier() functions

--- code/functions/plot_functions.R
+++ code/functions/plot_functions.R
@@ -18,3 +18,38 @@ histogram <- function(data, vector_name, var, grid_y, grid_x) {
     ggplot2::facet_grid(vars(.data[[grid_y]]), vars(.data[[grid_x]])) +
     labs(title = vector_name)
 }
+
+box_plot <- function(data, x, y, placement) {
+  # box_plot plots a boxplot of y by x, grouping by vector (resultant and 
+  # vertical) and labeling outliers.
+  #
+  # Args:
+  #   data: A data frame.
+  #   x, y: The name of the variable (unquoted) to be plot in the x and y axis.
+  #   placement: A character string with the name of the accelerometer
+  #   placement (either "ankle", "low back" or "hip").
+  #
+  # Returns:
+  #   A ggplot object.
+  data <- dplyr::mutate(
+    dplyr::group_by(data, {{ x }}, vector),
+    outlier = ifelse(is_outlier({{ y }}), subj, NA)
+  )
+  ggplot2::ggplot(data = data, aes(x = {{ x }}, y = {{ y }}, fill = vector)) +
+    ggplot2::geom_boxplot() +
+    ggrepel::geom_text_repel(
+      aes(label = outlier), na.rm = TRUE, position = position_dodge(1)
+    )
+}
+
+is_outlier <- function(x) {
+  # is_outlier checks whether the values of a variable are outliers.
+  #
+  # Args:
+  #   x: A numeric vector.
+  #
+  # Returns:
+  #   The x value when it is an outlier
+  x < quantile(x, 0.25, na.rm = TRUE) - 1.5 * IQR(x, na.rm = TRUE) | 
+  x > quantile(x, 0.75, na.rm = TRUE) + 1.5 * IQR(x, na.rm = TRUE)
+}
