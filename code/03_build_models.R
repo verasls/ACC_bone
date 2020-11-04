d39f1e00093443ecee09b2063e725b7405531a9b
Lucas <lucasdsveras@gmail.com>
2020-11-04 17:32:11 +0000

    Build and explore running ground reaction force and loading rate prediction models

--- code/03_build_models.R
+++ code/03_build_models.R
@@ -8,6 +8,7 @@ source(here("code", "functions", "model_functions.R"))
 
 # Load data ---------------------------------------------------------------
 
+running_df <- read_csv(here("data", "running_data.csv"))
 jumping_df <- read_csv(here("data", "jumping_data.csv"))
 
 # Build models ------------------------------------------------------------
@@ -16,10 +17,17 @@ model_df <- data.frame(
   vector = c(rep("resultant", 3), rep("vertical", 3)),
   placement = rep(c("ankle", "lower_back", "hip"), 2)
 )
-jump_data <- map2(
+
+running_data <- map2(
+  model_df$vector, model_df$placement, 
+  ~ filter_data(running_df, .x, .y)
+)
+
+jumping_data <- map2(
   model_df$vector, model_df$placement, 
   ~ filter_data(jumping_df, .x, .y)
 )
+
 GRF_formula <- as.formula(
   "pGRF_N ~ pACC_g + I(pACC_g^2) + body_mass + body_mass:pACC_g + (1 | subj)"
 )
@@ -27,22 +35,42 @@ LR_formula <- as.formula(
   "pLR_Ns ~ pATR_gs + I(pATR_gs^2) + body_mass + body_mass:pATR_gs + (1 | subj)"
 )
 
-# Jumping GRF
+# Running GRF
 # Build models
-GRF_models_jumping <- map(jump_data, ~ lmer(GRF_formula, .x)) %>% 
+GRF_models_running <- map(running_data, ~ lmer(GRF_formula, .x)) %>% 
   set_names(paste0(model_df$placement, "_", model_df$vector))
 # Compute accuracy indices
-GRF_accuracy_jumping <- map2(jump_data, GRF_models_jumping, accuracy) %>% 
+GRF_accuracy_running <- map2(running_data, GRF_models_running, accuracy) %>% 
+  set_names(paste0(model_df$placement, "_", model_df$vector))
+# Extract coefficients
+GRF_coefficients_running <- map(GRF_models_running, get_coefficients)
+
+# Running LR
+# Build models
+LR_models_running <- map(running_data, ~ lmer(LR_formula, .x)) %>% 
+  set_names(paste0(model_df$placement, "_", model_df$vector))
+# Compute accuracy indices
+LR_accuracy_running <- map2(running_data, LR_models_running, accuracy) %>% 
+  set_names(paste0(model_df$placement, "_", model_df$vector))
+# Extract coefficients
+LR_coefficients_running <- map(LR_models_running, get_coefficients)
+
+# Jumping GRF
+# Build models
+GRF_models_jumping <- map(jumping_data, ~ lmer(GRF_formula, .x)) %>% 
+  set_names(paste0(model_df$placement, "_", model_df$vector))
+# Compute accuracy indices
+GRF_accuracy_jumping <- map2(jumping_data, GRF_models_jumping, accuracy) %>% 
   set_names(paste0(model_df$placement, "_", model_df$vector))
 # Extract coefficients
 GRF_coefficients_jumping <- map(GRF_models_jumping, get_coefficients)
 
 # Jumping LR
 # Build models
-LR_models_jumping <- map(jump_data, ~ lmer(LR_formula, .x)) %>% 
+LR_models_jumping <- map(jumping_data, ~ lmer(LR_formula, .x)) %>% 
   set_names(paste0(model_df$placement, "_", model_df$vector))
 # Compute accuracy indices
-LR_accuracy_jumping <- map2(jump_data, LR_models_jumping, accuracy) %>% 
+LR_accuracy_jumping <- map2(jumping_data, LR_models_jumping, accuracy) %>% 
   set_names(paste0(model_df$placement, "_", model_df$vector))
 # Extract coefficients
 LR_coefficients_jumping <- map(LR_models_jumping, get_coefficients)
