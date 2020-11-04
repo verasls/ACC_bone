a65684799914bbc1371f5791db80ca58165a3f70
Lucas <lucasdsveras@gmail.com>
2020-11-04 19:21:29 +0000

    Remove accuracy() and add loocv_accuracy()

--- code/functions/model_functions.R
+++ code/functions/model_functions.R
@@ -1,30 +1,3 @@
-accuracy <- function(data, model) {
-  # accuracy computed some indices of accuracy of a model and builds a
-  # Bland-Altman plot
-  #
-  # Args:
-  #   data: A data frame.
-  #   model: A lmerMod object.
-  #
-  # Returns:
-  #   A list with two elements: the accuracy indices in a data frame and
-  #   the Bland-Altman plot in a ggplot object.
-  source(here("code", "functions", "plot_functions.R"))
-  outcome <- as.character(formula.tools::lhs(stats::formula(model)))
-  actual <- data[[outcome]]
-  predicted <- predict(model, data)
-  
-  R2 <- piecewiseSEM::rsquared(model)[[6]]
-  MAE <- lvmisc::mean_error_abs(actual, predicted)
-  MAPE <- lvmisc::mean_error_abs_pct(actual, predicted)
-  RMSE <- lvmisc::mean_error_sqr_root(actual, predicted)
-  
-  list(
-    indices = data.frame(R2, MAE, MAPE, RMSE),
-    plot = bland_altman(actual, predicted)
-  )
-}
-
 get_coefficients <- function(model) {
   # get_coefficients extract coefficients from a model.
   #
@@ -35,4 +8,40 @@ get_coefficients <- function(model) {
   #   A named numeric vector with the model coefficients.
   coefficients <- stats::coef(summary(model))
   coefficients[, 1]
+}
+
+# Define functions to extract data sets
+get_training_data <- function(x) analysis(x)
+get_testing_data <- function(x) assessment(x)
+
+loocv_accuracy <- function(data, formula) {
+  # loocv cross-validates the model using the leave-one-out cross-validation
+  #  (LOOCV) approach and compute some accuracy indices.
+  #
+  # Args:
+  #   data: A data frame.
+  #   formula: The model formula.
+  #
+  # Returns:
+  #   A data frame with the LOOCV accuracy indices.
+  outcome <- as.character(formula.tools::lhs(formula))
+  loocv_split <- rsample::group_vfold_cv(data, group = "subj")
+  training_data <- purrr::map(loocv_split$splits, get_training_data)
+  testing_data <- purrr::map(loocv_split$splits, get_testing_data)
+  
+  trained_models <- purrr::map(training_data, ~ lme4::lmer(formula, data = .x))
+  predicted <- purrr::map2(
+    trained_models, testing_data,
+    ~ stats::predict(.x, newdata = .y, allow.new.levels = TRUE)
+  ) %>% 
+    purrr::as_vector() %>% 
+    unname()
+  actual <- purrr::map(testing_data, outcome) %>% 
+    purrr::as_vector()
+  
+  R2 <- piecewiseSEM::rsquared(lme4::lmer(GRF_formula, data = data))[[6]]
+  MAE <- lvmisc::mean_error_abs(actual, predicted)
+  MAPE <- lvmisc::mean_error_abs_pct(actual, predicted)
+  RMSE <- lvmisc::mean_error_sqr_root(actual, predicted)
+  data.frame(R2, MAE, MAPE, RMSE)
 }
