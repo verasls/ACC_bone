ed2d9df22c87f31fab9dfc03b158cad5f4d3b989
Lucas <lucasdsveras@gmail.com>
2020-11-04 11:34:06 +0000

    Add filter_data()

--- code/functions/utils.R
+++ code/functions/utils.R
@@ -1,6 +1,5 @@
 my_correlate <- function(data, vector_name, placement) {
-  # my_correlate filters data by accelerometer placement and vector and then
-  # performs a correlation analysis of the selected variables.
+  # my_correlate performs a correlation analysis of the selected variables.
   #
   # Args:
   #   data: A data frame.
@@ -17,4 +16,19 @@ my_correlate <- function(data, vector_name, placement) {
     dplyr::select(pGRF_N, pACC_g, body_mass, pLR_Ns, pATR_gs) %>% 
     corrr::correlate()
   return(list(msg, corr))
+}
+
+filter_data <- function(data, vector_name, placement) {
+  # filter_data filters data by accelerometer placement and vector.
+  #
+  # Args:
+  #   data: A data frame.
+  #   vector_name: A character string with the vector name. Either "resultant"
+  #   or "vertical".
+  #   placement: A character string with the name of the accelerometer
+  #   placement (either "ankle", "low_back" or "hip").
+  #
+  # Returns:
+  #   A data frame.
+  dplyr::filter(data, vector == vector_name, acc_placement == placement)
 }
