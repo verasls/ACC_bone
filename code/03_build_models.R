44352bf7cf6727c6664f6e848ada6b60870ff634
Lucas <lucasdsveras@gmail.com>
2020-11-05 16:51:56 +0000

    Add bland_altman() function

--- code/03_build_models.R
+++ code/03_build_models.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(lme4)
 source(here("code", "functions", "utils.R"))
 source(here("code", "functions", "model_functions.R"))
+source(here("code", "functions", "plot_functions.R"))
 
 # Load data ---------------------------------------------------------------
 
@@ -17,6 +18,8 @@ model_df <- data.frame(
   vector = c(rep("resultant", 3), rep("vertical", 3)),
   placement = rep(c("ankle", "lower_back", "hip"), 2)
 )
+
+names <- paste0(model_df$placement, "_", model_df$vector)
 
 running_data <- map2(
   model_df$vector, model_df$placement, 
@@ -38,63 +41,71 @@ LR_formula <- as.formula(
 # Running GRF
 # Build models
 GRF_models_running <- map(running_data, ~ lmer(GRF_formula, .x)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Cross validate (leave-one-out cross-validation)
 GRF_loocv_running <- map(running_data, ~ loocv(.x, GRF_formula)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Compute accuracy indices
 GRF_accuracy_running <- map2(
   running_data, GRF_loocv_running, 
   ~ accuracy(.x, GRF_formula, .y$actual, .y$predicted)
 ) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
+# Bland-Altman plots
+GRF_plots_running <- map2(GRF_loocv_running, names, bland_altman)
 # Extract coefficients
 GRF_coefficients_running <- map(GRF_models_running, get_coefficients)
 
 # Running LR
 # Build models
 LR_models_running <- map(running_data, ~ lmer(LR_formula, .x)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Cross validate (leave-one-out cross-validation)
 LR_loocv_running <- map(running_data, ~ loocv(.x, LR_formula)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Compute accuracy indices
 LR_accuracy_running <- map2(
   running_data, LR_loocv_running, 
   ~ accuracy(.x, LR_formula, .y$actual, .y$predicted)
 ) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
+# Bland-Altman plots
+LR_plots_running <- map2(LR_loocv_running, names, bland_altman)
 # Extract coefficients
 LR_coefficients_running <- map(LR_models_running, get_coefficients)
 
 # Jumping GRF
 # Build models
 GRF_models_jumping <- map(jumping_data, ~ lmer(GRF_formula, .x)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Cross validate (leave-one-out cross-validation)
 GRF_loocv_jumping <- map(jumping_data, ~ loocv(.x, GRF_formula)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Compute accuracy indices
 GRF_accuracy_jumping <- map2(
   running_data, GRF_loocv_jumping, 
   ~ accuracy(.x, GRF_formula, .y$actual, .y$predicted)
 ) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
+# Bland-Altman plots
+GRF_plots_jumping <- map2(GRF_loocv_jumping, names, bland_altman)
 # Extract coefficients
 GRF_coefficients_jumping <- map(GRF_models_jumping, get_coefficients)
 
 # Jumping LR
 # Build models
 LR_models_jumping <- map(jumping_data, ~ lmer(LR_formula, .x)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Cross validate (leave-one-out cross-validation)
 LR_loocv_jumping <- map(jumping_data, ~ loocv(.x, LR_formula)) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
 # Compute accuracy indices
 LR_accuracy_jumping <- map2(
   running_data, LR_loocv_jumping, 
   ~ accuracy(.x, LR_formula, .y$actual, .y$predicted)
 ) %>% 
-  set_names(paste0(model_df$placement, "_", model_df$vector))
+  set_names(names)
+# Bland-Altman plots
+LR_plots_jumping <- map2(LR_loocv_jumping, names, bland_altman)
 # Extract coefficients
 LR_coefficients_jumping <- map(LR_models_jumping, get_coefficients)
