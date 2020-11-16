be003206e252138dd469dcbbe7c41490992c0f54
Lucas <lucasdsveras@gmail.com>
2020-11-16 16:47:48 +0000

    Remove trailing whitespace

--- code/03_build_models.R
+++ code/03_build_models.R
@@ -22,12 +22,12 @@ model_df <- data.frame(
 names <- paste0(model_df$placement, "_", model_df$vector)
 
 running_data <- map2(
-  model_df$vector, model_df$placement, 
+  model_df$vector, model_df$placement,
   ~ filter_data(running_df, .x, .y)
 )
 
 jumping_data <- map2(
-  model_df$vector, model_df$placement, 
+  model_df$vector, model_df$placement,
   ~ filter_data(jumping_df, .x, .y)
 )
 
@@ -40,16 +40,16 @@ LR_formula <- as.formula(
 
 # Running GRF
 # Build models
-GRF_models_running <- map(running_data, ~ lmer(GRF_formula, .x)) %>% 
+GRF_models_running <- map(running_data, ~ lmer(GRF_formula, .x)) %>%
   set_names(names)
 # Cross validate (leave-one-out cross-validation)
-GRF_loocv_running <- map(running_data, ~ loocv(.x, GRF_formula)) %>% 
+GRF_loocv_running <- map(running_data, ~ loocv(.x, GRF_formula)) %>%
   set_names(names)
 # Compute accuracy indices
 GRF_accuracy_running <- map2(
-  running_data, GRF_loocv_running, 
+  running_data, GRF_loocv_running,
   ~ accuracy(.x, GRF_formula, .y$actual, .y$predicted)
-) %>% 
+) %>%
   set_names(names)
 # Bland-Altman plots
 GRF_plots_running <- map2(GRF_loocv_running, names, bland_altman)
@@ -58,16 +58,16 @@ GRF_coefficients_running <- map(GRF_models_running, get_coefficients)
 
 # Running LR
 # Build models
-LR_models_running <- map(running_data, ~ lmer(LR_formula, .x)) %>% 
+LR_models_running <- map(running_data, ~ lmer(LR_formula, .x)) %>%
   set_names(names)
 # Cross validate (leave-one-out cross-validation)
-LR_loocv_running <- map(running_data, ~ loocv(.x, LR_formula)) %>% 
+LR_loocv_running <- map(running_data, ~ loocv(.x, LR_formula)) %>%
   set_names(names)
 # Compute accuracy indices
 LR_accuracy_running <- map2(
-  running_data, LR_loocv_running, 
+  running_data, LR_loocv_running,
   ~ accuracy(.x, LR_formula, .y$actual, .y$predicted)
-) %>% 
+) %>%
   set_names(names)
 # Bland-Altman plots
 LR_plots_running <- map2(LR_loocv_running, names, bland_altman)
@@ -76,16 +76,16 @@ LR_coefficients_running <- map(LR_models_running, get_coefficients)
 
 # Jumping GRF
 # Build models
-GRF_models_jumping <- map(jumping_data, ~ lmer(GRF_formula, .x)) %>% 
+GRF_models_jumping <- map(jumping_data, ~ lmer(GRF_formula, .x)) %>%
   set_names(names)
 # Cross validate (leave-one-out cross-validation)
-GRF_loocv_jumping <- map(jumping_data, ~ loocv(.x, GRF_formula)) %>% 
+GRF_loocv_jumping <- map(jumping_data, ~ loocv(.x, GRF_formula)) %>%
   set_names(names)
 # Compute accuracy indices
 GRF_accuracy_jumping <- map2(
-  running_data, GRF_loocv_jumping, 
+  running_data, GRF_loocv_jumping,
   ~ accuracy(.x, GRF_formula, .y$actual, .y$predicted)
-) %>% 
+) %>%
   set_names(names)
 # Bland-Altman plots
 GRF_plots_jumping <- map2(GRF_loocv_jumping, names, bland_altman)
@@ -94,16 +94,16 @@ GRF_coefficients_jumping <- map(GRF_models_jumping, get_coefficients)
 
 # Jumping LR
 # Build models
-LR_models_jumping <- map(jumping_data, ~ lmer(LR_formula, .x)) %>% 
+LR_models_jumping <- map(jumping_data, ~ lmer(LR_formula, .x)) %>%
   set_names(names)
 # Cross validate (leave-one-out cross-validation)
-LR_loocv_jumping <- map(jumping_data, ~ loocv(.x, LR_formula)) %>% 
+LR_loocv_jumping <- map(jumping_data, ~ loocv(.x, LR_formula)) %>%
   set_names(names)
 # Compute accuracy indices
 LR_accuracy_jumping <- map2(
-  running_data, LR_loocv_jumping, 
+  running_data, LR_loocv_jumping,
   ~ accuracy(.x, LR_formula, .y$actual, .y$predicted)
-) %>% 
+) %>%
   set_names(names)
 # Bland-Altman plots
 LR_plots_jumping <- map2(LR_loocv_jumping, names, bland_altman)
@@ -116,7 +116,7 @@ vectors <- c("resultant", "vertical")
 test_formula <- as.formula("value ~ value_type * activity_type + (1 | subj)")
 
 # Running GRF
-GRF_test_running <- map_dfr(GRF_loocv_running, rbind) %>% 
+GRF_test_running <- map_dfr(GRF_loocv_running, rbind) %>%
   prepare_data()
 GRF_test_models_running <- map(
   vectors,
@@ -124,14 +124,14 @@ GRF_test_models_running <- map(
     test_formula,
     data = GRF_test_running %>% filter(vector == .x)
   )
-) %>% 
+) %>%
   map(
     ~ anova(.x, type = 3, test = "F")
-  ) %>% 
+  ) %>%
   set_names(vectors)
 
 # Running LR
-LR_test_running <- map_dfr(LR_loocv_running, rbind) %>% 
+LR_test_running <- map_dfr(LR_loocv_running, rbind) %>%
   prepare_data()
 LR_test_models_running <- map(
   vectors,
@@ -139,14 +139,14 @@ LR_test_models_running <- map(
     test_formula,
     data = LR_test_running %>% filter(vector == .x)
   )
-) %>% 
+) %>%
   map(
     ~ anova(.x, type = 3, test = "F")
-  ) %>% 
+  ) %>%
   set_names(vectors)
 
 # Jumping GRF
-GRF_test_jumping <- map_dfr(GRF_loocv_jumping, rbind) %>% 
+GRF_test_jumping <- map_dfr(GRF_loocv_jumping, rbind) %>%
   prepare_data()
 GRF_test_models_jumping <- map(
   vectors,
@@ -154,14 +154,14 @@ GRF_test_models_jumping <- map(
     test_formula,
     data = GRF_test_jumping %>% filter(vector == .x)
   )
-) %>% 
+) %>%
   map(
   ~ anova(.x, type = 3, test = "F")
-) %>% 
+) %>%
   set_names(vectors)
 
 # Jumping LR
-LR_test_jumping <- map_dfr(LR_loocv_jumping, rbind) %>% 
+LR_test_jumping <- map_dfr(LR_loocv_jumping, rbind) %>%
   prepare_data()
 LR_test_models_jumping <- map(
   vectors,
@@ -169,8 +169,8 @@ LR_test_models_jumping <- map(
     test_formula,
     data = LR_test_jumping %>% filter(vector == .x)
   )
-) %>% 
+) %>%
   map(
     ~ anova(.x, type = 3, test = "F")
-  ) %>% 
+  ) %>%
   set_names(vectors)
