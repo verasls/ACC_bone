9e97bafaef2a02868ee39b0fb1eb3b40ee3fcca5
Lucas <lucasdsveras@gmail.com>
2020-11-17 15:13:18 +0000

    Make acc boxplots and combine plots

--- figs/boxplot.R
+++ figs/boxplot.R
@@ -3,6 +3,7 @@
 library(here)
 library(tidyverse)
 library(ragg)
+library(patchwork)
 
 # Load data ---------------------------------------------------------------
 
@@ -11,7 +12,7 @@ running_df <- read_csv(here("data/running_data.csv")) %>%
     run = as_factor(paste0("running ", speed, "km/h")),
   ) %>%
   select(
-    subj, acc_placement, vector, run, pGRF_BW
+    subj, acc_placement, vector, run, pGRF_BW, pACC_g
   )
 jumping_df <- read_csv(here("data/jumping_data.csv")) %>%
   mutate(
@@ -19,7 +20,7 @@ jumping_df <- read_csv(here("data/jumping_data.csv")) %>%
     jump = fct_relevel(jump, "drop jumps 40cm", after = 7),
   ) %>%
   select(
-    subj, acc_placement, vector, jump, pGRF_BW
+    subj, acc_placement, vector, jump, pGRF_BW, pACC_g
   )
 
 data <- running_df %>%
@@ -44,7 +45,7 @@ data <- running_df %>%
 
 # GRF boxplot -------------------------------------------------------------
 
-boxplot <- data %>%
+boxplot_GRF <- data %>%
   filter(acc_placement == "lower_back") %>%
   ggplot(aes(x = activity, y = pGRF_BW)) +
   geom_boxplot(aes(fill = vector), outlier.shape = NA) +
@@ -58,16 +59,127 @@ boxplot <- data %>%
   theme_light() +
   theme(
     axis.text.x = element_text(angle = 45, hjust = 1),
-    legend.title = element_blank()
+    legend.title = element_blank(),
+    legend.position = "top"
   ) +
   labs(x = "", y = "pGRF (BW)")
 
 agg_tiff(
-  here("figs/boxplot.tiff"),
-  width = 40,
+  here("figs/boxplot_GRF.tiff"),
+  width = 30,
   height = 20,
   units = "cm",
-  res = 300
+  res = 300,
+  scaling = 1.5
 )
-plot(boxplot)
+plot(boxplot_GRF)
+dev.off()
+
+# ACC boxplots ------------------------------------------------------------
+
+boxplot_ACC_ankle <- data %>%
+  filter(acc_placement == "ankle") %>%
+  ggplot(aes(x = activity, y = pACC_g)) +
+  geom_boxplot(aes(fill = vector), outlier.shape = NA) +
+  geom_jitter(size = 0.4, alpha = 0.6) +
+  scale_fill_manual(values = c("white", "gray")) +
+  scale_y_continuous(
+    limits = c(0, 16),
+    expand = c(0, 0),
+    breaks = seq(0, 16, 1)
+  ) +
+  theme_light() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.title = element_blank(),
+    legend.position = "none"
+  ) +
+  labs(x = "", y = "pACC (g)")
+
+boxplot_ACC_back <- data %>%
+  filter(acc_placement == "lower_back") %>%
+  ggplot(aes(x = activity, y = pACC_g)) +
+  geom_boxplot(aes(fill = vector), outlier.shape = NA) +
+  geom_jitter(size = 0.4, alpha = 0.6) +
+  scale_fill_manual(values = c("white", "gray")) +
+  scale_y_continuous(
+    limits = c(0, 13),
+    expand = c(0, 0),
+    breaks = seq(0, 13, 1)
+  ) +
+  theme_light() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.title = element_blank(),
+    legend.position = "none"
+  ) +
+  labs(x = "", y = "pACC (g)")
+
+boxplot_ACC_hip <- data %>%
+  filter(acc_placement == "hip") %>%
+  ggplot(aes(x = activity, y = pACC_g)) +
+  geom_boxplot(aes(fill = vector), outlier.shape = NA) +
+  geom_jitter(size = 0.4, alpha = 0.6) +
+  scale_fill_manual(values = c("white", "gray")) +
+  scale_y_continuous(
+    limits = c(0, 13),
+    expand = c(0, 0),
+    breaks = seq(0, 13, 1)
+  ) +
+  theme_light() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.title = element_blank(),
+    legend.position = "none"
+  ) +
+  labs(x = "", y = "pACC (g)")
+
+agg_tiff(
+  here("figs/boxplot_ACC_ankle.tiff"),
+  width = 30,
+  height = 20,
+  units = "cm",
+  res = 300,
+  scaling = 1.5
+)
+plot(boxplot_ACC_ankle)
+dev.off()
+
+agg_tiff(
+  here("figs/boxplot_ACC_back.tiff"),
+  width = 30,
+  height = 20,
+  units = "cm",
+  res = 300,
+  scaling = 1.5
+)
+plot(boxplot_ACC_back)
+dev.off()
+
+agg_tiff(
+  here("figs/boxplot_ACC_hip.tiff"),
+  width = 30,
+  height = 20,
+  units = "cm",
+  res = 300,
+  scaling = 1.5
+)
+plot(boxplot_ACC_hip)
+dev.off()
+
+# Combined GRF/ACC boxplot ------------------------------------------------
+
+a <- boxplot_GRF + boxplot_ACC_ankle + boxplot_ACC_back + boxplot_ACC_hip +
+  plot_layout(nrow = 1, byrow = FALSE, guides = "collect") &
+  theme(legend.position = "top")
+
+agg_tiff(
+  here("figs/boxplot.tiff"),
+  width = 120,
+  height = 20,
+  units = "cm",
+  res = 300,
+  scaling = 1.5
+)
+plot(a)
 dev.off()
