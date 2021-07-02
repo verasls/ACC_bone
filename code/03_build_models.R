c58a188dae619c6e11bc70732d2e3d99571e5103
Lucas <lucasdsveras@gmail.com>
2021-07-02 11:45:21 +0100

    Update models

--- code/03_build_models.R
+++ code/03_build_models.R
@@ -1,191 +1,132 @@
-# Load packages and functions ---------------------------------------------
+# Load packages -----------------------------------------------------------
 
 library(here)
 library(tidyverse)
 library(lme4)
-source(here("code/functions/utils.R"))
-source(here("code/functions/model_functions.R"))
-source(here("code/functions/plot_functions.R"))
+library(lvmisc)
+library(emmeans)
 
-# Load data ---------------------------------------------------------------
+# Load and prepare data ---------------------------------------------------
 
-running_df <- read_csv(here("data/processed/running_data.csv"))
-jumping_df <- read_csv(here("data/processed/jumping_data.csv"))
+load(here("data", "mechanical_load_data.rda"))
 
-# Build models ------------------------------------------------------------
+# Create a list of 3 data frames: one for each accelerometer placement
+mechanical_load_by_placements <- map(
+  c("ankle", "lower_back", "hip"),
+  ~ filter(mechanical_load_data, acc_placement == .x)
+) %>% set_names(c("ankle", "lower_back", "hip"))
 
-model_df <- data.frame(
-  vector = c(rep("resultant", 3), rep("vertical", 3)),
-  placement = rep(c("ankle", "lower_back", "hip"), 2)
+# Filter the data frames in the list, separating the vertical and resultant
+# vectors
+res_mechanical_load_data <- map(
+  mechanical_load_by_placements, filter, vector == "resultant"
+)
+ver_mechanical_load_data <- map(
+  mechanical_load_by_placements, filter, vector == "vertical"
 )
 
-names <- paste0(model_df$placement, "_", model_df$vector)
-
-running_data <- map2(
-  model_df$vector, model_df$placement,
-  ~ filter_data(running_df, .x, .y)
-)
-
-jumping_data <- map2(
-  model_df$vector, model_df$placement,
-  ~ filter_data(jumping_df, .x, .y)
-)
+# Build GRF models --------------------------------------------------------
 
 GRF_formula <- as.formula(
-  "pGRF_N ~ pACC_g + body_mass + body_mass:pACC_g + (1 | subj)"
+  "pGRF_N ~ pACC_g + body_mass + pACC_g:body_mass +
+    (1 | subj) + (1 | jump_height)"
 )
+
+# Resultant vector
+# Build models
+res_GRF_models <- map(
+  res_mechanical_load_data,
+  ~ lmer(GRF_formula, data = .x)
+)
+# Cross-validate (leave-one-out cross-validation)
+cv_res_GRF_models <- map2(
+  res_GRF_models, res_mechanical_load_data,
+  ~ loo_cv(.x, .y, id = subj)
+)
+# Compute accuracy indices
+accuracy_res_GRF_models <- map(cv_res_GRF_models, accuracy, na.rm = TRUE)
+# Bland-Altman plots
+plot_res_GRF_models <- map(
+  cv_res_GRF_models, plot_bland_altman, color = BMI_cat
+)
+
+# Vertical vector
+# Build models
+ver_GRF_models <- map(
+  ver_mechanical_load_data,
+  ~ lmer(GRF_formula, data = .x)
+)
+# Cross-validate (leave-one-out cross-validation)
+cv_ver_GRF_models <- map2(
+  ver_GRF_models, ver_mechanical_load_data,
+  ~ loo_cv(.x, .y, id = subj)
+)
+# Compute accuracy indices
+accuracy_ver_GRF_models <- map(cv_ver_GRF_models, accuracy, na.rm = TRUE)
+# Bland-Altman plots
+plot_ver_GRF_models <- map(
+  cv_ver_GRF_models, plot_bland_altman, color = BMI_cat
+)
+
+# Build LR models ---------------------------------------------------------
+
 LR_formula <- as.formula(
-  "pLR_Ns ~ pATR_gs + body_mass + body_mass:pATR_gs + (1 | subj)"
+  "pLR_Ns ~ pAR_gs + body_mass + pAR_gs:body_mass +
+    (1 | subj) + (1 | jump_height)"
 )
 
-# Running GRF
+# Resultant vector
 # Build models
-GRF_models_running <- map(running_data, ~ lmer(GRF_formula, .x)) %>%
-  set_names(names)
-# Cross validate (leave-one-out cross-validation)
-GRF_loocv_running <- map(running_data, ~ loocv(.x, GRF_formula)) %>%
-  set_names(names)
+res_LR_models <- map(
+  res_mechanical_load_data,
+  ~ lmer(LR_formula, data = .x)
+)
+# Cross-validate (leave-one-out cross-validation)
+cv_res_LR_models <- map2(
+  res_LR_models, res_mechanical_load_data,
+  ~ loo_cv(.x, .y, id = subj)
+)
 # Compute accuracy indices
-GRF_accuracy_running <- map2(
-  running_data, GRF_loocv_running,
-  ~ accuracy(.x, GRF_formula, .y$actual, .y$predicted)
-) %>%
-  set_names(names)
+accuracy_res_LR_models <- map(cv_res_LR_models, accuracy, na.rm = TRUE)
 # Bland-Altman plots
-GRF_plots_running <- map2(GRF_loocv_running, names, bland_altman)
-# Extract coefficients
-GRF_coefficients_running <- map(GRF_models_running, get_coefficients)
+plot_res_LR_models <- map(
+  cv_res_LR_models, plot_bland_altman, color = BMI_cat
+)
 
-# Running LR
+# Vertical vector
 # Build models
-LR_models_running <- map(running_data, ~ lmer(LR_formula, .x)) %>%
-  set_names(names)
-# Cross validate (leave-one-out cross-validation)
-LR_loocv_running <- map(running_data, ~ loocv(.x, LR_formula)) %>%
-  set_names(names)
+ver_LR_models <- map(
+  ver_mechanical_load_data,
+  ~ lmer(LR_formula, data = .x)
+)
+# Cross-validate (leave-one-out cross-validation)
+cv_ver_LR_models <- map2(
+  ver_LR_models, ver_mechanical_load_data,
+  ~ loo_cv(.x, .y, id = subj)
+)
 # Compute accuracy indices
-LR_accuracy_running <- map2(
-  running_data, LR_loocv_running,
-  ~ accuracy(.x, LR_formula, .y$actual, .y$predicted)
-) %>%
-  set_names(names)
+accuracy_ver_LR_models <- map(cv_ver_LR_models, accuracy, na.rm = TRUE)
 # Bland-Altman plots
-LR_plots_running <- map2(LR_loocv_running, names, bland_altman)
-# Extract coefficients
-LR_coefficients_running <- map(LR_models_running, get_coefficients)
+plot_ver_LR_models <- map(
+  cv_ver_LR_models, plot_bland_altman, color = BMI_cat
+)
 
-# Jumping GRF
-# Build models
-GRF_models_jumping <- map(jumping_data, ~ lmer(GRF_formula, .x)) %>%
-  set_names(names)
-# Cross validate (leave-one-out cross-validation)
-GRF_loocv_jumping <- map(jumping_data, ~ loocv(.x, GRF_formula)) %>%
-  set_names(names)
-# Compute accuracy indices
-GRF_accuracy_jumping <- map2(
-  running_data, GRF_loocv_jumping,
-  ~ accuracy(.x, GRF_formula, .y$actual, .y$predicted)
-) %>%
-  set_names(names)
-# Bland-Altman plots
-GRF_plots_jumping <- map2(GRF_loocv_jumping, names, bland_altman)
-# Extract coefficients
-GRF_coefficients_jumping <- map(GRF_models_jumping, get_coefficients)
+# Save models and leave-one-out cross-validation data ---------------------
 
-# Jumping LR
-# Build models
-LR_models_jumping <- map(jumping_data, ~ lmer(LR_formula, .x)) %>%
-  set_names(names)
-# Cross validate (leave-one-out cross-validation)
-LR_loocv_jumping <- map(jumping_data, ~ loocv(.x, LR_formula)) %>%
-  set_names(names)
-# Compute accuracy indices
-LR_accuracy_jumping <- map2(
-  jumping_data, LR_loocv_jumping,
-  ~ accuracy(.x, LR_formula, .y$actual, .y$predicted)
-) %>%
-  set_names(names)
-# Bland-Altman plots
-LR_plots_jumping <- map2(LR_loocv_jumping, names, bland_altman)
-# Extract coefficients
-LR_coefficients_jumping <- map(LR_models_jumping, get_coefficients)
-
-# Test differences between actual and predicted values --------------------
-
-vectors <- c("resultant", "vertical")
-test_formula <- as.formula("value ~ value_type * activity_type + (1 | subj)")
-
-# Running GRF
-GRF_test_running <- map_dfr(GRF_loocv_running, rbind) %>%
-  prepare_data()
-GRF_test_models_running <- map(
-  vectors,
-  ~ lmer(
-    test_formula,
-    data = GRF_test_running %>% filter(vector == .x)
-  )
-) %>%
-  map(
-    ~ anova(.x, type = 3, test = "F")
-  ) %>%
-  set_names(vectors)
-
-# Running LR
-LR_test_running <- map_dfr(LR_loocv_running, rbind) %>%
-  prepare_data()
-LR_test_models_running <- map(
-  vectors,
-  ~ lmer(
-    test_formula,
-    data = LR_test_running %>% filter(vector == .x)
-  )
-) %>%
-  map(
-    ~ anova(.x, type = 3, test = "F")
-  ) %>%
-  set_names(vectors)
-
-# Jumping GRF
-GRF_test_jumping <- map_dfr(GRF_loocv_jumping, rbind) %>%
-  prepare_data()
-GRF_test_models_jumping <- map(
-  vectors,
-  ~ lmer(
-    test_formula,
-    data = GRF_test_jumping %>% filter(vector == .x)
-  )
-) %>%
-  map(
-  ~ anova(.x, type = 3, test = "F")
-) %>%
-  set_names(vectors)
-
-# Jumping LR
-LR_test_jumping <- map_dfr(LR_loocv_jumping, rbind) %>%
-  prepare_data()
-LR_test_models_jumping <- map(
-  vectors,
-  ~ lmer(
-    test_formula,
-    data = LR_test_jumping %>% filter(vector == .x)
-  )
-) %>%
-  map(
-    ~ anova(.x, type = 3, test = "F")
-  ) %>%
-  set_names(vectors)
-
-# Write LOOCV data --------------------------------------------------------
-
-loocv_df <- list(
-  GRF_loocv_running = GRF_loocv_running,
-  GRF_loocv_jumping = GRF_loocv_jumping,
-  LR_loocv_running = LR_loocv_running,
-  LR_loocv_jumping = LR_loocv_jumping
-) %>%
-  map(bind_data)
-
-walk2(
-  loocv_df, names(loocv_df),
-  ~ write_data(.x, .y, here("data/processed/"))
+if (!dir.exists(here("output"))) {
+  dir.create(here("output"))
+}
+save(
+  cv_res_GRF_models, cv_ver_GRF_models,
+  cv_res_LR_models, cv_ver_LR_models,
+  file = here("output", "loocv_data.rda")
+)
+save(
+  res_GRF_models, ver_GRF_models,
+  res_LR_models, ver_LR_models,
+  accuracy_res_GRF_models,
+  accuracy_ver_GRF_models,
+  accuracy_res_LR_models,
+  accuracy_ver_LR_models,
+  file = here("output", "prediction_models.rda")
 )
