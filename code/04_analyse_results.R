8d482b5707879ef75217f8611a94ac5d750466a2
Lucas <lucasdsveras@gmail.com>
2021-07-13 19:07:43 +0100

    Add LR accuracy per jump type and save the objects

--- code/04_analyse_results.R
+++ code/04_analyse_results.R
@@ -8,7 +8,6 @@ source(here("code", "funs.R"))
 # Load and prepare data ---------------------------------------------------
 
 load(here("output", "loocv_data.rda"))
-load(here("data", "mechanical_load_data.rda"))
 
 # Bland-Altman plots tests ------------------------------------------------
 
@@ -27,23 +26,56 @@ ba_bias_test_ver_LR <- map(cv_ver_LR_models, bland_altman_t_test)
 # Accuracy per activity ---------------------------------------------------
 
 # Drop jumps
-drop_jumps_res_accuracy <- cv_res_GRF_models %>%
+drop_jumps_res_GRF_accuracy <- cv_res_GRF_models %>%
   map(~ filter(.x, jump_type == "drop jumps")) %>%
   map(accuracy, na.rm = TRUE)
-drop_jumps_ver_accuracy <- cv_ver_GRF_models %>%
+drop_jumps_ver_GRF_accuracy <- cv_ver_GRF_models %>%
+  map(~ filter(.x, jump_type == "drop jumps")) %>%
+  map(accuracy, na.rm = TRUE)
+drop_jumps_res_LR_accuracy <- cv_res_LR_models %>%
+  map(~ filter(.x, jump_type == "drop jumps")) %>%
+  map(accuracy, na.rm = TRUE)
+drop_jumps_ver_LR_accuracy <- cv_ver_LR_models %>%
   map(~ filter(.x, jump_type == "drop jumps")) %>%
   map(accuracy, na.rm = TRUE)
 # Box jumps
-box_jumps_res_accuracy <- cv_res_GRF_models %>%
+box_jumps_res_GRF_accuracy <- cv_res_GRF_models %>%
   map(~ filter(.x, jump_type == "box jumps")) %>%
   map(accuracy, na.rm = TRUE)
-box_jumps_ver_accuracy <- cv_ver_GRF_models %>%
+box_jumps_ver_GRF_accuracy <- cv_ver_GRF_models %>%
+  map(~ filter(.x, jump_type == "box jumps")) %>%
+  map(accuracy, na.rm = TRUE)
+box_jumps_res_LR_accuracy <- cv_res_LR_models %>%
+  map(~ filter(.x, jump_type == "box jumps")) %>%
+  map(accuracy, na.rm = TRUE)
+box_jumps_ver_LR_accuracy <- cv_ver_LR_models %>%
   map(~ filter(.x, jump_type == "box jumps")) %>%
   map(accuracy, na.rm = TRUE)
 # Continuous jumps
-continuous_jumps_res_accuracy <- cv_res_GRF_models %>%
+continuous_jumps_res_GRF_accuracy <- cv_res_GRF_models %>%
   map(~ filter(.x, jump_type == "continuous jumps")) %>%
   map(accuracy, na.rm = TRUE)
-continuous_jumps_ver_accuracy <- cv_ver_GRF_models %>%
+continuous_jumps_ver_GRF_accuracy <- cv_ver_GRF_models %>%
   map(~ filter(.x, jump_type == "continuous jumps")) %>%
   map(accuracy, na.rm = TRUE)
+continuous_jumps_res_LR_accuracy <- cv_res_LR_models %>%
+  map(~ filter(.x, jump_type == "continuous jumps")) %>%
+  map(accuracy, na.rm = TRUE)
+continuous_jumps_ver_LR_accuracy <- cv_ver_LR_models %>%
+  map(~ filter(.x, jump_type == "continuous jumps")) %>%
+  map(accuracy, na.rm = TRUE)
+
+# Save accuracy per activity ----------------------------------------------
+
+if (!dir.exists(here("output"))) {
+  dir.create(here("output"))
+}
+save(
+  drop_jumps_ver_GRF_accuracy, drop_jumps_ver_GRF_accuracy,
+  drop_jumps_res_LR_accuracy, drop_jumps_ver_LR_accuracy,
+  box_jumps_ver_GRF_accuracy, box_jumps_ver_GRF_accuracy,
+  box_jumps_res_LR_accuracy, box_jumps_ver_LR_accuracy,
+  continuous_jumps_ver_GRF_accuracy, continuous_jumps_ver_GRF_accuracy,
+  continuous_jumps_res_LR_accuracy, continuous_jumps_ver_LR_accuracy,
+  file = here("output", "jump_type_accuracy.rda")
+)
