01daa483b753ffcfd443ac2481457571ce1040c4
Lucas <lucasdsveras@gmail.com>
2021-07-13 11:20:14 +0100

    Reorder jump factor levels

--- code/01_tidy_data.R
+++ code/01_tidy_data.R
@@ -24,6 +24,9 @@ mechanical_load_data <- read_csv(here("data", "jumping_data.csv")) |>
       str_to_sentence(paste0(jump_type, " ", jump_height, "cm"))
     ),
     jump = fct_relevel(jump, "Drop jumps 40cm", after = 7),
+    jump = fct_relevel(
+      jump, "Continuous jumps 5cm", "Continuous jumps 15cm", after = Inf
+    ),
     .after = jump_height
   )
 
